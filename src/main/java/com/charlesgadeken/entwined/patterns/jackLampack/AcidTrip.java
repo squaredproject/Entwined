8063d1d8f68cc379b685363c9a065858d0d80d5b
Gerald Spencer <gerald.spencer@strateos.com>
2020-11-19 20:05:51 -0800

    Add jack lampack patterns

---
+++ src/main/java/com/charlesgadeken/entwined/patterns/jackLampack/AcidTrip.java
@@ -0,0 +1,33 @@
+package com.charlesgadeken.entwined.patterns.jackLampack;
+
+import com.charlesgadeken.entwined.Utilities;
+import com.charlesgadeken.entwined.model.BaseCube;
+import com.charlesgadeken.entwined.patterns.EntwinedBasePattern;
+import heronarts.lx.LX;
+import heronarts.lx.LXCategory;
+import heronarts.lx.modulator.SawLFO;
+
+@LXCategory("Jack Lampack")
+public class AcidTrip extends EntwinedBasePattern {
+
+  final SawLFO trails = new SawLFO(360, 0, 7000);
+
+  public AcidTrip(LX lx) {
+    super(lx);
+
+    addModulator(trails).start();
+  }
+
+  @Override
+  public void run(double deltaMs) {
+    if (getChannel().fader.getNormalized() == 0) return;
+
+    for (BaseCube cube : model.baseCubes) {
+      colors[cube.index] = lx.hsb(
+          Utilities.abs(model.cy - cube.transformedY) + Utilities.abs(model.cy - cube.transformedTheta) + trails.getValuef() % 360,
+          100,
+          100
+      );
+    }
+  }
+}
