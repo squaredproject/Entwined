1490f5c21803fc2c8d7bcabf13b228e49ba715d7
meawoppl <meawoppl@gmail.com>
2020-11-26 12:00:10 -0800

    Spotless:

--- src/main/java/com/charlesgadeken/entwined/effects/TurnOffDeadPixelsEffect.java
+++ src/main/java/com/charlesgadeken/entwined/effects/TurnOffDeadPixelsEffect.java
@@ -3,19 +3,18 @@ package com.charlesgadeken.entwined.effects;
 import heronarts.lx.LX;
 
 public class TurnOffDeadPixelsEffect extends EntwinedBaseEffect {
-    int[] deadPixelIndices = new int[] { };
-    int[] deadPixelClusters = new int[] { };
+    int[] deadPixelIndices = new int[] {};
+    int[] deadPixelClusters = new int[] {};
 
     TurnOffDeadPixelsEffect(LX lx) {
         super(lx);
     }
 
-    public void run(double deltaMs, double unused   ) {
+    public void run(double deltaMs, double unused) {
         for (int i = 0; i < deadPixelIndices.length; i++) {
-            //Cluster cluster = model.clusters.get(deadPixelClusters[i]);
-            //Cube cube = cluster.cubes.get(deadPixelIndices[i]);
-            //colors[cube.index] = LXColor.BLACK;
+            // Cluster cluster = model.clusters.get(deadPixelClusters[i]);
+            // Cube cube = cluster.cubes.get(deadPixelIndices[i]);
+            // colors[cube.index] = LXColor.BLACK;
         }
     }
 }
-
