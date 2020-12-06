b5a764aaef1d31959dc7cbec6173e1ed4b8c1681
meawoppl <meawoppl@gmail.com>
2020-12-05 21:25:59 -0800

    Remove dead flesh

--- src/main/java/com/charlesgadeken/entwined/model/ShrubCube.java
+++ src/main/java/com/charlesgadeken/entwined/model/ShrubCube.java
@@ -6,7 +6,7 @@ import toxi.geom.Vec3D;
 public class ShrubCube extends BaseCube {
     // Tiny cubes actually have less, but for Entwined we want to
     // tell the NDB that everything is 6
-    public static final int[] PIXELS_PER_CUBE = { 6, 6, 6, 12, 12 };
+    public static final int[] PIXELS_PER_CUBE = {6, 6, 6, 12, 12};
     public static final float[] CUBE_SIZES = {4f, 7.5f, 11.25f, 15f, 16.5f};
     /** Size of this cube, one of SMALL/MEDIUM/LARGE/GIANT */
     public final float size;
