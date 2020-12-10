509bdc966a37085082caa1ccdaec8b5ab2b5684f
Brian Bulkowski <brian@bulkowski.org>
2020-12-10 02:54:50 -0800

    Moved the filename for the autoplay into the config because we'll want that sooner rather than later

--- oldlx/Trees/Config.java
+++ oldlx/Trees/Config.java
@@ -3,7 +3,7 @@ final class Config {
   static final boolean autoplayBMSet = false;
 
   static final boolean enableNFC = false;
-  static final boolean enableAPC40 = false;
+  static final boolean enableAPC40 = true;
   static final boolean enableSoundSyphon = false;
 
   static final boolean enableOutputMinitree = false;
@@ -14,4 +14,7 @@ final class Config {
   static final String TREE_CONFIG_FILE = "data/entwinedTrees.json";
   static final String SHRUB_CUBE_CONFIG_FILE = "data/entwinedShrubCubes.json";
   static final String SHRUB_CONFIG_FILE = "data/entwinedShrubs.json";
+
+  // if this file doesn't exist you get a crash
+  static final String AUTOPLAY_FILE = "data/Burning Man Playlist.json";
 }
