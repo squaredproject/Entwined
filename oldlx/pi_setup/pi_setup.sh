f56e275eb7eb48a6d8e656cb512cc7f25501f345
Pouneh Mortazavi <pouneh@google.com>
2021-04-25 16:18:59 -0700

    fixed frame rate again

--- oldlx/pi_setup/pi_setup.sh
+++ oldlx/pi_setup/pi_setup.sh
@@ -102,7 +102,7 @@ sudo netfilter-persistent save
 ####### Slow frame ###
 ####### Rate Fix   ###
 ######################
-sudo sh -c 'sudo echo "net.ipv4.neigh.eth0.unres_qlen=1" >  /etc/sysctl.conf '
+sudo sh -c 'sudo echo "net.ipv4.neigh.eth0.unres_qlen=1" >>  /etc/sysctl.conf '
 sudo sh -c 'echo "net.ipv4.neigh.eth0.unres_qlen_bytes=4096" >>  /etc/sysctl.conf '
 
 ### Uncomment line re IP forwarding
