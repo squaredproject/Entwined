4c39eeb8ef6ae8e3a8d1ccc07a504fac921403df
Your Name <you@example.com>
2021-05-03 21:29:56 -0700

    added dos2unix utility

--- oldlx/pi_setup/pi_setup.sh
+++ oldlx/pi_setup/pi_setup.sh
@@ -62,13 +62,7 @@ sudo systemctl start  lx-headless
 sudo systemctl enable  brightness-toggle
 sudo systemctl start  brightness-toggle
 
-#####################
-####### Slow frame ###
-####### Rate Fix   ###
-######################
-sudo sh -c 'sudo echo "net.ipv4.neigh.eth0.unres_qlen=1" >  /etc/sysctl.conf '
-sudo sh -c 'echo "net.ipv4.neigh.eth0.unres_qlen_bytes=4096" >>  /etc/sysctl.conf '
-
+sudo apt-get install dos2unix
 
 ######################
 ####### Network ######
