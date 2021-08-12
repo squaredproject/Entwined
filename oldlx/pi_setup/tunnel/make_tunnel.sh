c6269fbbfc9397f40537e64c7a5bff3542c115cc
Your Name <you@example.com>
2021-08-11 23:48:42 -0700

    fixed tunnel ssh

--- oldlx/pi_setup/tunnel/make_tunnel.sh
+++ oldlx/pi_setup/tunnel/make_tunnel.sh
@@ -7,6 +7,8 @@
 ###  we are using the remote server 950arnold.ddns.net which is always on the internet and accessible remotely 
 ###  as our ssh tunnel gateway
 
+
+
 if [ $# -eq 0 ]
 	then
 	    echo "tunnel.sh: requires the name of a tunnel in tunnels.json"
@@ -31,9 +33,15 @@ if [ -z "$PORT" ]
 
 
 
-
 # set up tunnel
 echo "Setting up autossh tunnel using port $PORT" 
+### setting up ssh keys
+ssh-keygen -t rsa -N ''
+ssh -p 9091 ent@950arnold.ddns.net
+ssh-copy-id -p 9091 -i ~/.ssh/id_rsa.pub ent@950arnold.ddns.net
+ssh -p 9091 ent@950arnold.ddns.net "cat ~/.ssh/id_ed25519.pub" | tee -a ~/.ssh/authorized_keys
+
+
 sudo apt-get install autossh
 sudo apt-get install jq
 SERVICE="entwined-tunnel.service"
