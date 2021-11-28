6b386a3131948888b26ec88bb7635014865dc30e
Brian Bulkowski <brian@bulkowski.org>
2021-11-27 22:31:13 -0800

    Update a few key installations with empty fairy circle configs

--- oldlx/installations/reno-red/Config.java
+++ oldlx/installations/reno-red/Config.java
@@ -33,6 +33,7 @@ final class Config {
   static final String TREE_CONFIG_FILE = "data/entwinedTrees.json";
   static final String SHRUB_CUBE_CONFIG_FILE = "data/entwinedShrubCubes.json";
   static final String SHRUB_CONFIG_FILE = "data/entwinedShrubs.json";
+  static final String FAIRY_CIRCLE_CONFIG_FILE = "data/entwinedFairyCircles.json";
 
   // if this file doesn't exist you get a crash
   static final String AUTOPLAY_FILE = "data/entwined2021.json";
