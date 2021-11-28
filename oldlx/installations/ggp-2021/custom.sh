ac1abbbc2bb97c691a45834c142db52bef348bee
mizpoon <34733668+mizpoon@users.noreply.github.com>
2021-11-28 13:43:25 -0800

    new custom.sh for ggp-2021

---
+++ oldlx/installations/ggp-2021/custom.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CRONSTRING=$(crontab -l)
+LIGHTS=$"lights_on"
+
+if [[ "$CRONSTRING" != *"$LIGHTS"* ]];  then
+        (crontab -l 2>/dev/null; echo "5 18 * * * /home/entwined/Entwined/ddptest/lights_on.sh") | crontab -
+        (crontab -l 2>/dev/null; echo "11 22 * * * cd /home/entwined/Entwined/ddptest/; ./lights_out.sh") | crontab -
+        echo "Creating cron jobs to turn lights on and off"
+fi
+
+
+echo "Adding statuscake"
+if [[ "$CRONSTRING" != *"statuscake"* ]];  then
+        (crontab -l 2>/dev/null; echo "*/5 * * * * /home/entwined/Entwined/oldlx/pi_setup/tunnel/statuscake.sh") | c$fi
