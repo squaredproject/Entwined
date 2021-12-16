2f01377a8f1910bbc9112cd58e267044af51c14e
Your Name <you@example.com>
2021-12-16 07:27:18 -0800

    removed mycron "turd"

--- oldlx/installations/reno-red/custom.sh
+++ oldlx/installations/reno-red/custom.sh
@@ -10,9 +10,9 @@ CAKE="https://push.statuscake.com/?PK=0f3ecb0c6b5698b&TestID=6083753&time=0"
 
 
 #write out current crontab
-crontab -l > mycron
+crontab -l > /tmp/mycron
 
 #echo new cron into cron file
-echo "*/5 * * * * /usr/bin/curl ${CAKE} >/tmp/curl 2>&1" >> mycron
+echo "*/5 * * * * /usr/bin/curl ${CAKE} >/tmp/curl 2>&1" >> /tmp/mycron
 #install new cron file
-crontab mycron
+crontab /tmp/mycron
