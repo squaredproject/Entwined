9d86edc80be71713fc411715ad1595268b2d318e
Charlie Stigler <charlie@charliestigler.com>
2022-02-04 01:27:17 -0800

    Add new interactive color effects Candy Chaos, Rainbow, and White

--- oldlx/Trees/Patterns_KyleFleming.java
+++ oldlx/Trees/Patterns_KyleFleming.java
@@ -1159,6 +1159,10 @@ class CandyTextureEffect extends Effect {
 
   double time = 0;
 
+  // if set to a value >= 0, the effects are limited to only
+  // the piece with that index (used for interactive effects)
+  protected int pieceIndex = -1;
+
   CandyTextureEffect(LX lx) {
     super(lx);
   }
@@ -1168,6 +1172,11 @@ class CandyTextureEffect extends Effect {
       time += deltaMs;
       for (int i = 0; i < colors.length; i++) {
         int oldColor = colors[i];
+        BaseCube cube = model.baseCubes.get(i);
+
+        // if we're only applying this effect to a given pieceIndex, filter out and don't set colors on other cubes
+        if (pieceIndex >= 0 && cube.pieceIndex != pieceIndex) continue;
+
         float newHue = i * 127 + 9342 + (float)time % 360;
         int newColor = lx.hsb(newHue, 100, 100);
         int blendedColor = LXColor.lerp(oldColor, newColor, amount.getValuef());
@@ -1185,6 +1194,10 @@ class CandyCloudTextureEffect extends Effect {
   final double scale = 2400;
   final double speed = 1.0f / 5000;
 
+  // if set to a value >= 0, the effects are limited to only
+  // the piece with that index (used for interactive effects)
+  protected int pieceIndex = -1;
+
   CandyCloudTextureEffect(LX lx) {
     super(lx);
   }
@@ -1195,6 +1208,9 @@ class CandyCloudTextureEffect extends Effect {
       for (int i = 0; i < colors.length; i++) {
         int oldColor = colors[i];
         BaseCube cube = model.baseCubes.get(i);
+
+        // if we're only applying this effect to a given pieceIndex, filter out and don't set colors on other cubes
+        if (pieceIndex >= 0 && cube.pieceIndex != pieceIndex) continue;
 
         double adjustedX = cube.x / scale;
         double adjustedY = cube.y / scale;
