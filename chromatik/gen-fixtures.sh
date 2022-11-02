518f538b3ab2cb8c123c19b08b64a377b2ef98f2
Mark Slee <mcslee@gmail.com>
2022-11-02 09:31:10 -0700

    Move fixture generation stuff into chromatik folder, avoid symlink relative dir confusion

--- chromatik/gen-fixtures.sh
+++ chromatik/gen-fixtures.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -f src/main/resources/fixtures/*.lxf
-python3 ../newlx/sculpture/fixtures/shrub.py ../newlx/sculpture/fixtures/examples/entwined_2021/entwinedShrubs.json src/main/resources/fixtures
-python3 ../newlx/sculpture/fixtures/fairy_circle.py ../newlx/sculpture/fixtures/examples/entwined_2021/entwinedFairyCircles.json src/main/resources/fixtures
-python3 ../newlx/sculpture/fixtures/trees.py ../newlx/sculpture/fixtures/examples/entwined_2021/entwinedTrees.json ../newlx/sculpture/fixtures/examples/entwined_2021/cubes.csv --folder src/main/resources/fixtures
-rm -fr ../newlx/sculpture/fixtures/fixtures
+python3 sculpture/fixtures/shrub.py sculpture/fixtures/examples/entwined_2021/entwinedShrubs.json src/main/resources/fixtures
+python3 sculpture/fixtures/fairy_circle.py sculpture/fixtures/examples/entwined_2021/entwinedFairyCircles.json src/main/resources/fixtures
+python3 sculpture/fixtures/trees.py sculpture/fixtures/examples/entwined_2021/entwinedTrees.json sculpture/fixtures/examples/entwined_2021/cubes.csv --folder src/main/resources/fixtures
+rm -fr sculpture/fixtures/fixtures
