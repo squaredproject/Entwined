ab25dd4fcdf9f6c8943b7b3db1d47eb49c61105a
Mark Slee <mcslee@gmail.com>
2022-11-02 09:56:36 -0700

    Intall script to bootstrap Chromatik folder

--- chromatik/install.sh
+++ chromatik/install.sh
@@ -2,6 +2,7 @@
 mvn clean
 mvn validate
 mvn package
+mkdir -p ~/Chromatik/Packages
 cp target/entwined-0.0.1-SNAPSHOT.jar ~/Chromatik/Packages
 mkdir -p ~/Chromatik/Fixtures/Entwined
 cp src/main/resources/fixtures/* ~/Chromatik/Fixtures/Entwined
