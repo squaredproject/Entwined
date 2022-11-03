e22b671c2c6f23357a8700ae0eafecb398508ab7
Carolyn Wales <cswales@gmail.com>
2022-11-02 22:40:36 -0700

    adding a few simple patterns that the trigger code seemed to want

---
+++ chromatik/src/main/java/entwined/pattern/kyle_fleming/NoPattern.java
@@ -0,0 +1,15 @@
+package entwined.pattern.kyle_fleming;
+
+import heronarts.lx.pattern.LXPattern;
+import heronarts.lx.LX;
+
+
+public class NoPattern extends LXPattern {
+  public NoPattern(LX lx) {
+    super(lx);
+  }
+
+  @Override
+  public void run(double deltaMs) {
+  }
+}
