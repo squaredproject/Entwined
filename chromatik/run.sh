0ce114fcd2dba5ffa4753d7a40428ac6febb8924
Mark Slee <mcslee@gmail.com>
2022-11-03 08:42:58 -0700

    Duplicate mvn package and install in run.sh script

--- chromatik/run.sh
+++ chromatik/run.sh
@@ -1,1 +1,7 @@
+#!/bin/sh
+mvn package
+mkdir -p ~/Chromatik/Packages
+cp target/entwined-0.0.1-SNAPSHOT.jar ~/Chromatik/Packages
+mkdir -p ~/Chromatik/Fixtures/Entwined
+cp src/main/resources/fixtures/* ~/Chromatik/Fixtures/Entwined
 java -XstartOnFirstThread -cp lib/glxstudio-0.4.2-SNAPSHOT-jar-with-dependencies.jar heronarts.lx.studio.Chromatik --warnings --disable-zeroconf --enable-plugin entwined.plugin.Entwined src/main/resources/projects/Entwined-2022.lxp
