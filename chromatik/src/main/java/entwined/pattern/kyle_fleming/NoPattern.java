b0258e50cff4c37a6b6e2e629a22158d1b7e48d8
Carolyn Wales <cswales@gmail.com>
2022-11-06 11:49:39 -0800

    fix naming of no pattern

--- chromatik/src/main/java/entwined/pattern/kyle_fleming/NoPattern.java
+++ chromatik/src/main/java/entwined/pattern/kyle_fleming/NoPattern.java
@@ -2,11 +2,13 @@ package entwined.pattern.kyle_fleming;
 
 import heronarts.lx.pattern.LXPattern;
 import heronarts.lx.LX;
+import heronarts.lx.LXComponentName;
 
-
+@LXComponentName("NoPattern")
 public class NoPattern extends LXPattern {
   public NoPattern(LX lx) {
     super(lx);
+    // label = new StringParameter("NoPattern", "Turns off all effects");
   }
 
   @Override
