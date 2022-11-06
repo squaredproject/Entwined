68242c6ceaa52e37b3e7901960aeaafca13c2a9d
Brian Bulkowski <brian@bulkowski.org>
2022-11-06 14:07:50 -0800

    Update run to do the right thing on mac and leave windows alone probably need more?

--- chromatik/run.sh
+++ chromatik/run.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 mvn package
 mkdir -p ~/Chromatik/Packages
 rm -f ~/Chromatik/Packages/entwined-0.0.1-SNAPSHOT.jar
 cp target/entwined-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/Chromatik/Packages
 mkdir -p ~/Chromatik/Fixtures/Entwined
 cp src/main/resources/fixtures/* ~/Chromatik/Fixtures/Entwined
-java -XstartOnFirstThread -cp lib/glxstudio-0.4.2-SNAPSHOT-jar-with-dependencies.jar heronarts.lx.studio.Chromatik --warnings --disable-zeroconf --enable-plugin entwined.plugin.Entwined src/main/resources/projects/Entwined-2022.lxp
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+	$RUNOPT = "-XstartOnFirstThread"
+fi
+
+java $RUNOPT -cp lib/glxstudio-0.4.2-SNAPSHOT-jar-with-dependencies.jar heronarts.lx.studio.Chromatik --warnings --disable-zeroconf --enable-plugin entwined.plugin.Entwined src/main/resources/projects/Entwined-2022.lxp
