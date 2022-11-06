14b2e1e380f1bd0419d77bbebf619dcf01ae8675
Brian Bulkowski <brian@bulkowski.org>
2022-11-06 14:06:13 -0800

    Update the headless script to do the right thing on mac vs linux

--- chromatik/headless.sh
+++ chromatik/headless.sh
@@ -1,1 +1,11 @@
-java -XstartOnFirstThread -cp lib/glxstudio-0.4.2-SNAPSHOT-jar-with-dependencies.jar heronarts.lx.studio.Chromatik --warnings --disable-zeroconf --enable-plugin entwined.plugin.Entwined --disable-prefences --headless src/main/resources/projects/Entwined-2022.lxp
+#!/bin/bash
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+        RUNOPT="-XstartOnFirstThread"
+fi
+
+if [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
+	JARTYPE="-linux"
+fi
+
+java $RUNOPT -cp lib/glxstudio-0.4.2-SNAPSHOT-jar-with-dependencies$JARTYPE.jar heronarts.lx.studio.Chromatik --warnings --disable-zeroconf --enable-plugin entwined.plugin.Entwined --disable-prefences --headless src/main/resources/projects/Entwined-2022.lxp
