40606cdcb514da8cc3df4a28cd11f072bd089885
Brian Bulkowski <brian@bulkowski.org>
2022-11-06 17:37:17 -0800

    Added a powershell script to install fixtures too

---
+++ chromatik/installations/install_fixtures.ps1
@@ -0,0 +1,27 @@
+#!/usr/bin/env pwsh
+
+param ($install_dir)
+# powershell knows ~ but python doesn't so use $HOME
+$fixtures_dir = "$HOME/Chromatik/Fixtures/Entwined/"
+
+if (!$install_dir) {
+        echo "required parameter: directory to install"
+        exit
+}
+
+echo "making fixtures from directory: $install_dir WILL REMOVE EXISTING ENTWINED FIXTURES"
+
+if (! ( Test-Path -Path $install_dir )) {
+        echo "directory does not exist, try again"
+        exit
+}
+
+
+
+mkdir -p $fixtures_dir -ea 0
+rm $fixtures_dir/*
+
+echo "building LXF files from JSON descriptions"
+python fairy_circle.py --config $install_dir/fairy_circles.json --fixtures_folder $fixtures_dir
+python shrub.py --config $install_dir/shrubs.json --fixtures_folder $fixtures_dir
+python tree.py --tree_config $install_dir/trees.json --branch_config $install_dir/tree_branches.csv --fixtures_folder $fixtures_dir
