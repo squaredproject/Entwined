00fc9667c6b3e9b87c45fc7689283243aebb4dd8
Brian Bulkowski <brian@bulkowski.org>
2022-11-13 21:51:50 -0800

    Forgot to update the path in the headless file

--- chromatik/headless.sh
+++ chromatik/headless.sh
@@ -8,4 +8,4 @@ if [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
 	JARTYPE="-linux"
 fi
 
-java $RUNOPT -cp lib/glxstudio-0.4.2-SNAPSHOT-jar-with-dependencies$JARTYPE.jar heronarts.lx.studio.Chromatik --warnings --disable-zeroconf --enable-plugin entwined.plugin.Entwined --disable-prefences --headless ~/Chromatik/entwined.lxp
+java $RUNOPT -cp lib/glxstudio-0.4.2-SNAPSHOT-jar-with-dependencies$JARTYPE.jar heronarts.lx.studio.Chromatik --warnings --disable-zeroconf --enable-plugin entwined.plugin.Entwined --disable-prefences --headless ~/Chromatik/Projects/entwined.lxp
