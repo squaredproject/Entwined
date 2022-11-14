ec0d05cd0d785604825559d98c12cb1ec071d00d
Brian Bulkowski <brian@bulkowski.org>
2022-11-13 23:09:55 -0800

    Had to add a delay while at startup time to remove a dependancy or something

--- chromatik/headless.sh
+++ chromatik/headless.sh
@@ -8,4 +8,7 @@ if [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
 	JARTYPE="-linux"
 fi
 
+# there is a startup  problem with licenses so a small delay allows it to work at boot time - remove when fixed
+sleep 10
+
 java $RUNOPT -cp lib/glxstudio-0.4.2-SNAPSHOT-jar-with-dependencies$JARTYPE.jar heronarts.lx.studio.Chromatik --warnings --disable-zeroconf --enable-plugin entwined.plugin.Entwined --disable-prefences --headless /home/pi/Chromatik/Projects/entwined.lxp
