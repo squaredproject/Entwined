78b5232cfbf008d972dda1de6438d50ff9fbd44f
Brian Bulkowski <brian@bulkowski.org>
2022-11-13 16:32:08 -0800

    Make the install be the default, install production which installs the extra production monitoring requires more thought

--- chromatik/installations/install.sh
+++ chromatik/installations/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "installing from directory: $1 (remove existing), and installing crontab: statuscake and ddptest etc"
+echo "making fixtures from directory: $1 WILL REMOVE EXISTING ENTWINED FIXTURES"
 
 if [[ ! -d $1 ]]
 then
@@ -15,8 +15,3 @@ echo "building LXF files from JSON descriptions"
 python fairy_circle.py --config $1/fairy_circles.json --fixtures_folder ~/Chromatik/Fixtures/Entwined
 python shrub.py --config $1/shrubs.json --fixtures_folder ~/Chromatik/Fixtures/Entwined
 python tree.py --tree_config $1/trees.json --branch_config $1/tree_branches.csv --fixtures_folder ~/Chromatik/Fixtures/Entwined
-
-if test -f "${1}/custom.sh"; then
-    echo "installing custom configuration (statuscake ddptest whatever)"
-    $1/custom.sh $1
-fi
--- chromatik/installations/install_production.sh
+++ chromatik/installations/install_production.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "making fixtures from directory: $1 WILL REMOVE EXISTING ENTWINED FIXTURES"
+echo "installing from directory: $1 (remove existing), and installing crontab: statuscake and ddptest etc"
 
 if [[ ! -d $1 ]]
 then
@@ -15,3 +15,8 @@ echo "building LXF files from JSON descriptions"
 python fairy_circle.py --config $1/fairy_circles.json --fixtures_folder ~/Chromatik/Fixtures/Entwined
 python shrub.py --config $1/shrubs.json --fixtures_folder ~/Chromatik/Fixtures/Entwined
 python tree.py --tree_config $1/trees.json --branch_config $1/tree_branches.csv --fixtures_folder ~/Chromatik/Fixtures/Entwined
+
+if test -f "${1}/custom.sh"; then
+    echo "installing custom configuration (statuscake ddptest whatever)"
+    $1/custom.sh $1
+fi
