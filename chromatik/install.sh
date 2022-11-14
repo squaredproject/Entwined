85a92ecb9f768d25e31f3d5b15e41df30d1cc2c0
Brian Bulkowski <brian@bulkowski.org>
2022-11-13 17:01:13 -0800

    Change these to also be executable

--- chromatik/install.sh
+++ chromatik/install.sh
@@ -5,6 +5,8 @@ mvn package
 mkdir -p ~/Chromatik/Packages
 rm -f ~/Chromatik/Packages/entwined-0.0.1-SNAPSHOT.jar
 cp target/entwined-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/Chromatik/Packages
-mkdir -p ~/Chromatik/Fixtures/Entwined
-cp src/main/resources/fixtures/* ~/Chromatik/Fixtures/Entwined
+
+#fixtures are now copied from the installations directory, not from entwined
+#mkdir -p ~/Chromatik/Fixtures/Entwined
+#cp src/main/resources/fixtures/* ~/Chromatik/Fixtures/Entwined
 
--- chromatik/run.sh
+++ chromatik/run.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+
+# no longer compile on every run
 # mvn package
+
 mkdir -p ~/Chromatik/Packages
 rm -f ~/Chromatik/Packages/entwined-0.0.1-SNAPSHOT.jar
 cp target/entwined-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/Chromatik/Packages
-mkdir -p ~/Chromatik/Fixtures/Entwined
-cp src/main/resources/fixtures/* ~/Chromatik/Fixtures/Entwined
+
+#fixtures are now copied using the installation folder
+#mkdir -p ~/Chromatik/Fixtures/Entwined
+#cp src/main/resources/fixtures/* ~/Chromatik/Fixtures/Entwined
 
 if [[ $OSTYPE == 'darwin'* ]]; then
   RUNOPT="-XstartOnFirstThread"
