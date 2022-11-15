218d08307e5f04000bbe58e432003dd54f01b075
Eric Gauderman <eric.gauderman@gmail.com>
2022-11-15 00:30:09 -0800

    Hide error from removing old jar in install.ps1

--- chromatik/install.ps1
+++ chromatik/install.ps1
@@ -3,7 +3,7 @@ mvn clean
 mvn validate
 mvn package
 mkdir -p ~/Chromatik/Packages -ea 0
-rm -Force ~/Chromatik/Packages/entwined-0.0.1-SNAPSHOT.jar
+rm -Erroraction 'silentlycontinue' -Force ~/Chromatik/Packages/entwined-0.0.1-SNAPSHOT.jar
 cp target/entwined-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/Chromatik/Packages
 mkdir -p ~/Chromatik/Fixtures/Entwined -ea 0
 cp src/main/resources/fixtures/* ~/Chromatik/Fixtures/Entwined
