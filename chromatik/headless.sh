dc1ee1f09caccbe9d77bae76f028ebb18dcda9d5
Mark Slee <mcslee@gmail.com>
2022-11-14 19:44:20 -0800

    Fix --disable-preferences typo

--- chromatik/headless.sh
+++ chromatik/headless.sh
@@ -11,4 +11,4 @@ fi
 # there is a startup  problem with licenses so a small delay allows it to work at boot time - remove when fixed
 sleep 10
 
-java $RUNOPT -cp lib/glxstudio-0.4.2-SNAPSHOT-jar-with-dependencies$JARTYPE.jar heronarts.lx.studio.Chromatik --warnings --disable-zeroconf --enable-plugin entwined.plugin.Entwined --disable-prefences --headless /home/pi/Chromatik/Projects/entwined.lxp
+java $RUNOPT -cp lib/glxstudio-0.4.2-SNAPSHOT-jar-with-dependencies$JARTYPE.jar heronarts.lx.studio.Chromatik --warnings --disable-zeroconf --enable-plugin entwined.plugin.Entwined --disable-preferences --headless /home/pi/Chromatik/Projects/entwined.lxp
