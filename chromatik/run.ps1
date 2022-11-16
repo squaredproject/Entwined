c24b88f289b3f1c8f71e28302a97ff7972de4d7c
Brian Bulkowski <brian@bulkowski.org>
2022-11-15 22:22:35 -0800

    Don't copy from the checked in fixtures, do it from the installation directories

--- chromatik/run.ps1
+++ chromatik/run.ps1
@@ -7,7 +7,7 @@ rm -Force -ea 0 ~/Chromatik/Packages/entwined-0.0.1-SNAPSHOT.jar
 cp target/entwined-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/Chromatik/Packages
 
 # get the Fixtures and initial project with the installations scripts
-mkdir -p ~/Chromatik/Fixtures/Entwined -ea 0
-cp src/main/resources/fixtures/* ~/Chromatik/Fixtures/Entwined
+#mkdir -p ~/Chromatik/Fixtures/Entwined -ea 0
+#cp src/main/resources/fixtures/* ~/Chromatik/Fixtures/Entwined
 
 java -cp lib/glxstudio-0.4.2-SNAPSHOT-jar-with-dependencies-windows.jar heronarts.lx.studio.Chromatik --warnings --disable-zeroconf --enable-plugin entwined.plugin.Entwined ~/Chromatik/Projects/Entwined.lxp
