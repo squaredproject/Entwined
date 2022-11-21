db57fb0bb138ac0ef224b3cac6496073b1aa8179
Brian Bulkowski <brian@bulkowski.org>
2022-11-20 20:55:48 -0800

    Fix ability to start. Headless uses require license instead of blindly starting, and the service waits a few seconds before restarting. Haven't diagnosed what the issue with license on start up is though

--- chromatik/headless.sh
+++ chromatik/headless.sh
@@ -9,7 +9,7 @@ if [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
 fi
 
 # there is a startup  problem with licenses so a small delay allows it to work at boot time - remove when fixed
-sleep 10
+#sleep 10
 
 # note that since we are loading a plugin, we want to require a license, because we're expecting the plugin to work.
 java $RUNOPT -cp lib/glxstudio-0.4.2-SNAPSHOT-jar-with-dependencies$JARTYPE.jar heronarts.lx.studio.Chromatik --warnings --disable-zeroconf --require-license --enable-plugin entwined.plugin.Entwined --disable-preferences --headless /home/pi/Chromatik/Projects/entwined.lxp
