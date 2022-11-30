2fe18b916524f76fd8e1a7bb3263db0b895b2b74
Entwined Pi <mizpoon@burningart.com>
2022-11-30 13:32:21 -1000

    added lights on|off script

--- chromatik/installations/ggp-2022/custom.sh
+++ chromatik/installations/ggp-2022/custom.sh
@@ -7,3 +7,12 @@ if [[ "$CRONSTRING" != *"statuscake"* ]];  then
         (crontab -l 2>/dev/null; echo "*/5 * * * * /home/pi/Entwined/chromatik/installations/$1/statuscake.sh") | crontab -
 echo "Adding statuscake ping script"
 fi
+
+LIGHTS=$"lights_on"
+
+if [[ "$CRONSTRING" != *"$LIGHTS"* ]];  then
+    echo "NO CRON $CRONSTRING"
+        (crontab -l 2>/dev/null; echo "0 5 * * * cd /home/pi/Entwined/ddptest; ./lights_on.sh") | crontab -
+        (crontab -l 2>/dev/null; echo "0 22 * * * cd /home/pi/Entwined/ddptest/; ./lights_out.sh") | crontab -
+        echo "Creating cron jobs to turn lights on and off"
+fi
