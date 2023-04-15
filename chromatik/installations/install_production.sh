8ed4c08e2858ff3f756862e7851cc63c3b2a78b1
Entwined Pi <mizpoon@burningart.com>
2023-04-15 13:49:43 -0700

    fixing up installation scripts - install production now calls basic install, basic install does not do custom things

--- chromatik/installations/install_production.sh
+++ chromatik/installations/install_production.sh
@@ -8,19 +8,7 @@ then
         exit
 fi
 
-D="~$HOME/Chromatik"
-
-mkdir -p $D/Fixtures/Entwined
-rm -rf $D/Fixtures/Entwined/*
-rm -f $D/autoplay.lxr $D/Projects/entwined.lxp $D/config.json
-
-echo "building LXF files from JSON descriptions"
-python fairy_circle.py --config $1/fairy_circles.json --fixtures_folder $D/Fixtures/Entwined
-python shrub.py --config $1/shrubs.json --fixtures_folder $D/Fixtures/Entwined
-python tree.py --tree_config $1/trees.json --branch_config $D/tree_branches.csv --fixtures_folder $D/Fixtures/Entwined
-cp $1/entwined.lxp $D/Projects
-
-[[ -e $1/autoplay.lxr ]] && cp $1/autoplay.lxr $D
+./install.sh $1
 
 if test -f "${1}/custom.sh"; then
     echo "installing custom configuration (statuscake ddptest whatever)"
