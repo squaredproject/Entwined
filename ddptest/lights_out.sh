0043a911a771e92c0c64ee6509cefc4d52663975
Brian Bulkowski <brian@bulkowski.org>
2023-12-16 19:17:37 -0800

    Add a paramter to lights out so we can set to a color, default is still black Add a light blue to ddptest

--- ddptest/lights_out.sh
+++ ddptest/lights_out.sh
@@ -2,11 +2,16 @@
 
 sudo service chromatik stop
 #sudo nmap  -sn 10.0.0.100-255 -oG - | awk '/Up$/{print $2}' > /home/entwined/Entwined/ddptest/ndb_ips.txt
+
+if [ -z "$1" ]; then
+	$1 = "black"
+fi
+
 xs="1 2 3"
 for x in $xs; do
 	echo LIGHTS OUT $x	
 	cat ndb_ips.txt | while read ip; do
-	python3 ddptest.py --pattern black --leds 480 --host $ip
+	python3 ddptest.py --pattern $1 --leds 480 --host $ip
 done
 sleep 5
 done
