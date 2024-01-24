af13fbd9807a10c9d35f6e054f0aa5e08e1ef7dd
irenedesf <irenedesf@gmail.com>
2024-01-23 21:15:42 -0800

    cleanupfew

--- chromatik/src/main/java/entwined/pattern/irenedesf/zisdetestAll4.java
+++ chromatik/src/main/java/entwined/pattern/irenedesf/zisdetestAll4.java
@@ -46,6 +46,34 @@ public class zisdetestAll4 extends LXPattern {
         zstartloc = -180;
 
         if (time_elapsed > 9000) {
+            for (LXPoint cube : model.points) {
+                if (cube.x > -130 && cube.x < 0 && cube.y > 40 && cube.y < 400
+                    && cube.z > -20 && cube.z < 20) {
+                    // colors[cube.index] = LX.hsb(30, 75, 100);}
+                    // colors[cube1.index] = LX.hsb(30, 75, 100);}
+                    colors[cube1.index] = LX.hsb(colorOfCubes.getValuef(), 50,
+                        100);
+                } //
+            }
+            for (LXPoint cube2 : model.points) {
+                if (cube2.x > 0 && cube2.x < 130 && cube2.y > 40
+                    && cube2.y < 400 && cube2.z > -20 && cube2.z < 20) {
+                    // colors[cube2.index] = LX.hsb(30, 75, 100);}
+                    colors[cube2.index] = LX.hsb(colorOfCubes.getValuef(), 50,
+                        100);
+                } //
+            }
+            for (LXPoint cube3 : model.points) {
+                if (cube3.x > xstartloc && cube3.x < xstartloc + xwidth
+                    && cube3.y > 40 && cube3.y < 400 && cube3.z > -180
+                    && cube3.z < 0) {
+                    // colors[cube3.index] = LX.hsb(30, 75, 100);}
+                    colors[cube3.index] = LX.hsb(colorOfCubes.getValuef(), 50,
+                        100);
+                } //
+            }
+        }
+        else if (time_elapsed > 8000) {
             // This is where the colors happen.
             for (LXPoint cube : model.points) {
                 if (cube.x > -130 && cube.x < 0 && cube.y > 40 && cube.y < 400
@@ -56,7 +84,7 @@ public class zisdetestAll4 extends LXPattern {
                 } //
             }
         } /* time3000 */
-        else if (time_elapsed > 6000) {
+        else if (time_elapsed > 5000) {
             for (LXPoint cube1 : model.points) {
                 if (cube1.x > -30 && cube1.x < 30 && cube1.y > 40
                     && cube1.y < 400 && cube1.z > 30 && cube1.z < 130) {
@@ -65,7 +93,7 @@ public class zisdetestAll4 extends LXPattern {
                         100);
                 } //
             }
-        } else if (time_elapsed > 3000) {
+        } else if (time_elapsed > 2000) {
             for (LXPoint cube2 : model.points) {
                 if (cube2.x > 0 && cube2.x < 130 && cube2.y > 40
                     && cube2.y < 400 && cube2.z > -20 && cube2.z < 20) {
@@ -85,8 +113,10 @@ public class zisdetestAll4 extends LXPattern {
                 } //
             }
         } // else end
-        if (time_elapsed > 15000) {
+        if (time_elapsed > 12000) {
             time_elapsed = 0;
+
+
         }
 
     }
