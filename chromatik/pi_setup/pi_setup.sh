250aedf49610a2e5c310af130ae22c4db8ec55ad
CHARLES GADEKEN <charlesgadeken@Elder-Mother-2.monkeybrains.net>
2024-01-28 12:19:15 -0800

    added new debian12 networking, removed ipv6

--- chromatik/pi_setup/pi_setup.sh
+++ chromatik/pi_setup/pi_setup.sh
@@ -16,14 +16,21 @@ sudo apt-get update
 sudo apt-get dist-upgrade
 
 sudo apt-get install -y emacs dos2unix figlet
-echo "figlet \"entwined meadow\"" >> ~/.bash_profile
+
+
+### add login message
+MOTD="entwined meadow"
+BASH=`cat ~/.bash_profile`
+
+if ! [[ "$BASH" =~ $MOTD ]]; then
+    echo "figlet \"$MOTD\"" >> ~/.bash_profile
+fi
 
 #### fix any date issues
 ###  first configure locale using raspi-config 
 sudo apt install ntp -y 
 sudo apt install systemd-timesyncd -y
 sudo timedatectl set-ntp true -y
-# sudo apt install htpdate
 
 
 ######################
@@ -71,6 +78,9 @@ else
     sudo cat dhcpcd.conf >> /etc/dhcpcd.conf
 fi
 
+
+### disable ip6
+sudo cp ./97-entwined.conf /etc/sysctl.d/97-entwined.conf
 
 ###### NETWORKING
 
