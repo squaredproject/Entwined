76895ddd8e36305525b345956f8183939774777a
Brian Bulkowski <brian@bulkowski.org>
2024-05-07 17:26:39 -0700

    More complexity around Install to support the variance of MacOS and Linux python uses

--- chromatik/installations/install.sh
+++ chromatik/installations/install.sh
@@ -1,51 +1,76 @@
 #!/usr/bin/env bash
 
-echo "making fixtures from directory: $1 WILL REMOVE EXISTING ENTWINED FIXTURES"
+echo "making fixtures from directory: $1 WILL OVERWRITE EXISTING ENTWINED FIXTURES"
 
 if [[ ! -d $1 ]]
 then
-        echo "directory does not exist, try again"
+        echo "$1 directory does not exist, try again"
         exit
 fi
 
-I="$HOME/Chromatik"
+# I="$HOME/Chromatik"
 # Use this for WSL testing
-# I="/mnt/c/Users/bbulk/Chromatik"
+I="/mnt/c/Users/bbulk/Chromatik"
 
+# clean and remove the old things
 mkdir -p $I/Fixtures/Entwined
 [ -d $I/Projects ] || mkdir -p $I/Projects
 rm -rf $I/Fixtures/Entwined/*
 rm -f $I/autoplay.lxr $I/Projects/entwined.lxp $I/config.json
 
-echo "building LXF files from JSON descriptions"
-if [ -f $1/shrubs.json ]
+# deal with the major annoyance that is python best practices across
+# Macos Linux Windows
+
+if [ ! -z ${2} ];
 then
-  python3 shrub.py --config $1/shrubs.json --fixtures_folder $I/Fixtures/Entwined
+  PY=$2
+  echo "Python specified on the command line, using $PY"
+else  
+  # use python as the default
+  PY="python"
+  # but override with python3 if available
+  if command -v python3 &> /dev/null ; then
+    echo "python3 detected, using that instead of python"
+    PY="python3"
+  fi
 fi
 
-if [ -f $1/fairy_circles.json ]
-then
-  python3 fairy_circle.py --config $1/fairy_circles.json --fixtures_folder $I/Fixtures/Entwined
-fi
+# but if there's really no python, fail out
+if command -v $PY &> /dev/null ; then
 
-if [ -f $1/trees.json ]
-then
-  python3 tree.py --tree_config $1/trees.json --branch_config $1/tree_branches.csv --fixtures_folder $I/Fixtures/Entwined
-fi
+  echo "building LXF files from JSON descriptions using $PY"
+  if [ -f $1/shrubs.json ]
+  then
+    $PY shrub.py --config $1/shrubs.json --fixtures_folder $I/Fixtures/Entwined
+  fi
 
-if [ -f $1/bench.json ]
-then
-  python3 bench.py --config $1/bench.json --fixtures_folder $I/Fixtures/Entwined
-fi
+  if [ -f $1/fairy_circles.json ]
+  then
+    $PY fairy_circle.py --config $1/fairy_circles.json --fixtures_folder $I/Fixtures/Entwined
+  fi
 
-if [ -f $1/spots.json ]
-then
-  python3 spot.py --config $1/spots.json --fixtures_folder $I/Fixtures/Entwined
-fi
+  if [ -f $1/trees.json ]
+  then
+    $PY tree.py --tree_config $1/trees.json --branch_config $1/tree_branches.csv --fixtures_folder $I/Fixtures/Entwined
+  fi
 
-if [[ -f $1/elder_mother_cubes.csv ]]; then
-    python3 elder_mother.py --ndb_config $1/elder_ndb_ips.txt --cubes_config $1/elder_mother_cubes.csv --elder_config $1/elder_mother.json --fixtures_folder $I/Fixtures/Entwined/
-    echo "Created elder mother fixtures"
+  if [ -f $1/bench.json ]
+  then
+    $PY bench.py --config $1/bench.json --fixtures_folder $I/Fixtures/Entwined
+  fi
+
+  if [ -f $1/spots.json ]
+  then
+    $PY spot.py --config $1/spots.json --fixtures_folder $I/Fixtures/Entwined
+  fi
+
+  if [[ -f $1/elder_mother_cubes.csv ]]; then
+      python3 elder_mother.py --ndb_config $1/elder_ndb_ips.txt --cubes_config $1/elder_mother_cubes.csv --elder_config $1/elder_mother.json --fixtures_folder $I/Fixtures/Entwined/
+      echo "Created elder mother fixtures"
+  fi
+
+else
+  echo "No python detected, skipping building fixture files from json descriptions"
 fi
 
 cp $1/*.lxp $I/Projects
@@ -53,7 +78,9 @@ if [ -f $1/config.json ]; then
   cp $1/config.json $I
 fi
 
-cp $1/*.lxr $I 2>/dev/null
+# autoplay is read from the root but the program looks in projects by default
+cp $1/autoplay.lxr $I 2>/dev/null
+cp $1/*.lxr $I/Projects 2>/dev/null
 
 if [[ -e $1/Fixtures ]]; then
   cp -r $1/Fixtures/* $I/Fixtures
