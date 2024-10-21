3169b3d7ebde94ec968e703beef8a81a83ac3928
sambro20 <sam.brocchini@gmail.com>
2024-10-21 12:29:56 -0700

    new laguna install v2
    title

--- chromatik/installations/laguna-2024/statuscake.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-### status cake script
-### add to cron to run every 5 mins
-###  (crontab -l 2>/dev/null; echo "*/5 * * * * /home/entwined/Entwined/oldlx/installations/ggp-2021/statuscake.sh") | crontab -
-curl "https://push.statuscake.com/?PK=902984e591fe16a&TestID=5774810&time=0"
-echo `date`
